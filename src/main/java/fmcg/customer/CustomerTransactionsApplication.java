b34416c352cb093b30df46806a1dc253707f51be
babucs <m.babufastmail@gmail.com>
2018-02-12 11:19:06 +0530

    Initial commit

---
+++ src/main/java/fmcg/customer/CustomerTransactionsApplication.java
@@ -0,0 +1,12 @@
+package fmcg.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerTransactionsApplication.class, args);
+	}
+}
